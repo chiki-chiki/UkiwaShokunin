30adac6c4263271d64f08c63f855241541e6c1d1
UM_283\tsuba <basabasausausa@gmail.com>
2023-06-22 22:26:40 +0900

    firstcommit

---
+++ Assets/Scripts/AirInporter.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AirInporter : MonoBehaviour
+{
+    bool pushFrag = false;
+    float pushTime;
+    [SerializeField]float maxPushTime;
+
+    float airAmount;
+    [SerializeField] float maxAirAmount;
+    float airAmountDifference;
+
+    [SerializeField] Image ukiwa;
+
+    public static float score;
+    [SerializeField] Text scoreText;
+    [SerializeField] float okScore;
+    [SerializeField] float goodScore;
+    [SerializeField] float perfectScore;
+
+    float timeLimit;
+    [SerializeField]Text timeLimitText;
+    [SerializeField]float maxTimeLimit;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        timeLimit = maxTimeLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pushFrag)
+        {
+            pushTime += Time.deltaTime;
+            if (pushTime >= maxPushTime)
+            {
+                pushFrag = false;
+                pushTime = 0;
+            }
+            airAmount += Time.deltaTime;
+        }
+        ukiwa.fillAmount = airAmount / maxAirAmount;
+
+        
+        timeLimit -= Time.deltaTime;
+        timeLimitText.text = timeLimit.ToString("F1");
+
+        if (timeLimit < 0)
+        {
+            SceneManager.LoadScene("Result");
+        }
+
+    }
+
+    public void PushDown()
+    {
+        pushFrag = true;
+    }
+
+    public void PushUp()
+    {
+        pushFrag = false;
+        pushTime = 0;
+    }
+
+    public void Complete()
+    {
+        airAmountDifference = airAmount/maxAirAmount;
+        Debug.Log(airAmountDifference);
+
+        //空気8割以上でOK
+        if (airAmountDifference >= 0.8 && airAmountDifference < 0.9)
+        {
+            score += okScore;
+        }
+        else if (airAmountDifference >= 0.9 && airAmountDifference < 0.99)
+        {
+            score += goodScore;
+        }
+        else if (airAmountDifference >= 0.99&&airAmountDifference<=1.0)
+        {
+            score += perfectScore;
+        }
+        else if (airAmountDifference > 1.0)
+        {
+            score += 0;
+        }
+
+        scoreText.text = score.ToString();
+
+        pushFrag = false;
+        pushTime = 0;
+        airAmount = 0;
+    }
+}
---
+++ Assets/Scripts/Result.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour
+{
+    [SerializeField]Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = AirInporter.score.ToString();
+    }
+}
